1c06786751e1507d1fc376a3e8179e1ea02956ae
Witold Szumigała <witold.szumigala@gmail.com>
2025-03-30 20:23:58 +0200

    FIX: poprawka

--- android/src/main/java/com/jetbridge/reactobd2/OBD2Handler.java
+++ android/src/main/java/com/jetbridge/reactobd2/OBD2Handler.java
@@ -33,7 +33,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
+
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
--- android/src/main/java/com/jetbridge/reactobd2/ReactNativeOBD2Package.java
+++ android/src/main/java/com/jetbridge/reactobd2/ReactNativeOBD2Package.java
@@ -19,10 +19,10 @@ public class ReactNativeOBD2Package implements ReactPackage {
     return modules;
   }
 
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
+  // @Override
+  // public List<Class<? extends JavaScriptModule>> createJSModules() {
+  //   return Collections.emptyList();
+  // }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
